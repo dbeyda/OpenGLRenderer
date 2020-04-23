191aa757bcb8501d99f0545b144fbdc020e1a622
David Beyda <dbeyda@globo.com>
2020-04-23 03:34:53 -0300

    new: camera movements

--- PixelLighting/src/Application.cpp
+++ PixelLighting/src/Application.cpp
@@ -14,26 +14,48 @@
 #include "VertexBufferLayout.h"
 #include "Texture.h"
 #include "Obj.h"
+#include "Camera.h"
 
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
 
 
 int main(void)
 {
     std::string objPath = "";
     objPath = "res/models/golfball/golfball.obj";
-    // objPath = "res/models/formula 1/Formula 1 mesh.obj";
+    objPath = "res/models/formula 1/Formula 1 mesh.obj";
     // objPath = "res/models/stones/stones.obj";
 
     std::string texPath = "";
-    // texPath = "res/models/formula 1/Substance SpecGloss/Right ones/formula1_DefaultMaterial_Diffuse.png";
+    texPath = "res/models/formula 1/Substance SpecGloss/Right ones/formula1_DefaultMaterial_Diffuse.png";
     // texPath = "res/models/stones/stones.jpg";
     unsigned int texFormat = GL_RGB;
 
-    Obj obj(objPath);
-    obj.Load();
-
+    glm::vec3 cameraPos =       glm::vec3(0.0f, 0.0f, 5.0f);
+    glm::vec3 cameraFront =     glm::vec3(0.0f, 0.0f, -1.0f);
+    glm::vec3 cameraUp =        glm::vec3(0.0f, 1.0f, 0.0f);
+    
     GLFWwindow* window;
 
     /* Initialize the library */
@@ -45,12 +67,21 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(600, 600, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
+
+    /* set camera callbacks */
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    // tell GLFW to capture our mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
@@ -69,6 +100,9 @@ int main(void)
         GLCall(glEnable(GL_BLEND));
         GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
+        Obj obj(objPath);
+        obj.Load();
+
         VertexArray va;
         VertexBuffer vb(obj.positions.data() , obj.positions.size() * sizeof(float));
 
@@ -80,25 +114,16 @@ int main(void)
 
         IndexBuffer ib(obj.indices.data(), obj.indices.size());
 
-        glm::mat4 proj = glm::perspective(glm::radians(50.0f), 1.0f, 0.1f, 5000.0f);
-        glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 5.0f),  // eye
-                           glm::vec3(0.0f, 0.0f, 0.0f), // center
-                           glm::vec3(0.0f, 1.0f, 0.0f)   // up
-        );
-        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(-0.5f, -0.5f, 0));
-        //model = glm::scale(model, glm::vec3(10.0f));
 
         Texture texture(texPath, texFormat);
-
         if (texPath.size())
         {
             texture.Load();
             texture.Bind();
         }
 
-        // Shader shader("res/shaders/BasicTexture.shader");
-        Shader shader("res/shaders/Phong.shader");
-
+        Shader shader("res/shaders/BasicTexture.shader");
+        // Shader shader("res/shaders/Phong.shader");
 
         va.Unbind();
         vb.Unbind();
@@ -112,9 +137,22 @@ int main(void)
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
-            glm::mat4 rotatedModel = glm::rotate(model, glm::radians(rotation), glm::vec3(0, 1, 0));
+            // per-frame time logic
+            // --------------------
+            float currentFrame = glfwGetTime();
+            deltaTime = currentFrame - lastFrame;
+            lastFrame = currentFrame;
+
+            // input
+            // -----
+            processInput(window);
+
+            glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 5000.0f);
+            glm::mat4 view = camera.GetViewMatrix();
+            glm::mat4 rotatedModel = glm::rotate(glm::mat4(1.0f), glm::radians(rotation), glm::vec3(0, 1, 0));
             rotatedModel = glm::rotate(rotatedModel, glm::radians(rotation), glm::vec3(0, 1, 1));
-            glm::mat4 mvp = proj * view * rotatedModel;
+
+            glm::mat4 mvp = projection* view * rotatedModel;
 
             /* Render here */
             renderer.Clear();
@@ -135,4 +173,54 @@ int main(void)
     }
     glfwTerminate();
     return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    bool running = false;
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+        running = true;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime, running);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime, running);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime, running);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime, running);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(yoffset);
 }
