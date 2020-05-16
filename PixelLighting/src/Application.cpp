fc2cec765057dfa0cd6ce436ef1ed1f442cfac01
David Beyda <dbeyda@globo.com>
2020-05-16 20:30:15 -0300

    new: Multiple object rendering

--- PixelLighting/src/Application.cpp
+++ PixelLighting/src/Application.cpp
@@ -7,14 +7,9 @@
 #include <GLFW/glfw3.h>
 
 #include "Renderer.h"
-#include "VertexBuffer.h"
-#include "IndexBuffer.h"
-#include "VertexArray.h"
 #include "Shader.h"
-#include "VertexBufferLayout.h"
-#include "Texture.h"
-#include "Obj.h"
 #include "Camera.h"
+#include "Model.h"
 
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
@@ -46,29 +41,18 @@ float deltaTime = 0.0f;	// time between current frame and last frame
 float lastFrame = 0.0f;
 
 // lighting
-float globalLightStrength = 0.03f;
+float globalLightStrength = 0.0f;
 glm::vec4 globalLightColor(1.0f);
 
-float kc = 2;
-float kl = 0.01;
-float kq = 0.005;
-int sexp = 37;
-
-// material properties
-float mshi = 61.0;
-float ks = 3.0;
-float ka = 0.0;
-float kd = 4.6;
-int ilum = 2;
+float kc = 1;
+float kl = 0.005;
+float kq = 0.0001;
+int sexp = 30;
 
 
 int main(void)
 {
 	// system("PAUSE");
-	std::string objPath = "";
-	objPath = "res/models/golfball/golfball.obj";
-	// objPath = "res/models/stones/stones.obj";
-	// objPath = "res/models/formula 1/Formula 1 mesh.obj";
 
 	glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 5.0f);
 	glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
@@ -86,7 +70,7 @@ int main(void)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Unreal Engine 2053 secret preview", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -115,69 +99,32 @@ int main(void)
 	{
 		GLCall(glEnable(GL_BLEND));
 		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+		
+		Renderer renderer;
+		
+		std::string golfballPath = "res/models/golfball/golfball.obj";
+		Model golfball("Golfball", golfballPath, renderer);
+		golfball.m_Model = glm::translate(golfball.m_Model, glm::vec3(0, -7, 0));
+		
+		std::string stonesPath = "res/models/stones/stones.obj";
+		Model stones("Stones", stonesPath, renderer);
+		stones.m_Model = glm::translate(stones.m_Model, glm::vec3(-10, -10, -10));
+		stones.m_Model = glm::scale(stones.m_Model, glm::vec3(20, 20, 20));
 
-		Obj obj(objPath);
-		obj.Load();
+		Model stonesLeft("Stones", stonesPath, renderer);
+		stonesLeft.m_Model = glm::translate(glm::mat4(1.0f), glm::vec3(-10, -10, 10));
+		stonesLeft.m_Model = glm::scale(stonesLeft.m_Model, glm::vec3(20, 20, 20));
+		stonesLeft.m_Model = glm::rotate(stonesLeft.m_Model, glm::radians(90.0f), glm::vec3(0, 1, 0));
 
-		VertexArray va;
-		VertexBuffer vb(obj.positions.data(), obj.positions.size() * sizeof(float));
+		Model stonesBottom("Stones", stonesPath, renderer);
+		stonesBottom.m_Model = glm::translate(glm::mat4(1.0f), glm::vec3(-10, -10, 10));
+		stonesBottom.m_Model = glm::scale(stonesBottom.m_Model, glm::vec3(20, 20, 20));
+		stonesBottom.m_Model = glm::rotate(stonesBottom.m_Model, glm::radians(-90.0f), glm::vec3(1, 0, 0));
 
-		VertexBufferLayout layout;
-		layout.Push<float>(3); // position
-		layout.Push<float>(3); // normal vector
-		layout.Push<float>(2); // texture uv
-		layout.Push<float>(3); // tangent vector
-		va.AddBuffer(vb, layout);
 
-		IndexBuffer ib(obj.indices.data(), obj.indices.size());
-
+		std::vector<Model*> models({ &golfball, &stones, &stonesLeft, &stonesBottom});
 		Shader shader("res/shaders/PhongBumpMap.shader");
 		shader.Bind();
-		shader.SetUniform1i("u_hasAmbientTexture", 0);
-		shader.SetUniform1i("u_hasDiffuseTexture", 0);
-		shader.SetUniform1i("u_hasBumpTexture", 0);
-
-		Texture ambientTexture;
-		if (obj.materials.size() && obj.materials[0].ambient_texname.size())
-		{
-			ambientTexture.Load(obj.materials[0].ambient_texname);
-			shader.SetUniform1i("u_hasAmbientTexture", 1);
-			shader.SetUniform1i("AmbientTexture", 0);
-			ambientTexture.Bind();
-		}
-
-		Texture diffuseTexture;
-		if (obj.materials.size() && obj.materials[0].diffuse_texname.size())
-		{
-			diffuseTexture.Load(obj.materials[0].diffuse_texname, 1);
-			shader.SetUniform1i("u_hasDiffuseTexture", 1);
-			shader.SetUniform1i("DiffuseTexture", 1);
-			diffuseTexture.Bind();
-		}
-
-		Texture bumpTexture;
-		if (obj.materials.size() && obj.materials[0].bump_texname.size());
-		{
-			bumpTexture.Load(obj.materials[0].bump_texname, 2);
-			useBumpTexture = 1;
-			shader.SetUniform1i("u_hasBumpTexture", 1);
-			shader.SetUniform1i("BumpTexture", 2);
-			bumpTexture.Bind();
-		}
-		if (obj.materials.size())
-		{
-			tinyobj::material_t& mat = obj.materials[0];
-			mshi = mat.shininess;
-			ka = mat.ambient[0];
-			kd = mat.diffuse[0];
-			ks = mat.specular[0];
-
-		}
-		va.Unbind();
-		vb.Unbind();
-		ib.Unbind();
-
-		Renderer renderer;
 
 		float rotation = 0.0f;
 		float increment = 0.3f;
@@ -203,41 +150,48 @@ int main(void)
 			glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 5000.0f);
 			glm::mat4 view = camera.GetViewMatrix();
 
-			// Loaded Obj
-			glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-			glm::mat4 rotatedModel = glm::rotate(model, glm::radians(rotation), glm::vec3(1, 0, 0));
-			glm::mat4 mvp = projection * view * rotatedModel;
-			glm::mat4 mv = view * rotatedModel;
-			glm::mat4 invTransMv = glm::inverseTranspose(view * rotatedModel);
-
 			// Light
-			glm::mat4 lightModel = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 5));
+			glm::vec3 spotLightDir = glm::vec3(0, 0, 0);
+			glm::mat4 lightModel = glm::translate(glm::mat4(1.0f), glm::vec3(10, 0.5, 10));
 			glm::mat4 lightMv = view * lightModel;
+			glm::mat4 invTransMvLight = glm::inverseTranspose(lightMv);
+
 			glm::vec3 mvLightPos = lightMv * glm::vec4(0, 0, 0, 1);
-			
+			glm::vec3 mvSpotLightDir = invTransMvLight * glm::vec4(-1.5, -1, -1.5, 1);
+
 			/* Render here */
 			renderer.Clear();
 
 			shader.Bind();
-			shader.SetUniformMat4f("u_MVP", mvp);
-			shader.SetUniformMat4f("u_MV", mv);
-			shader.SetUniformMat4f("u_invTransMV", invTransMv);
-			shader.SetUniform3f("u_mvLightPos", mvLightPos.x, mvLightPos.y, mvLightPos.z);
-
 			shader.SetUniform4f("u_globalLightColor", globalLightColor.r, globalLightColor.g, globalLightColor.b, 1.0f);
 			shader.SetUniform1f("u_globalLightStrength", globalLightStrength);
-			shader.SetUniform1f("u_ka", ka);
-			shader.SetUniform1f("u_kd", kd);
+			shader.SetUniform3f("u_mvLightPos", mvLightPos.x, mvLightPos.y, mvLightPos.z);
+			shader.SetUniform3f("u_mvSpotLightDir", mvSpotLightDir.x, mvSpotLightDir.y, mvSpotLightDir.z);
 			shader.SetUniform1f("u_kc", kc);
 			shader.SetUniform1f("u_kl", kl);
 			shader.SetUniform1f("u_kq", kq);
 			shader.SetUniform1i("u_sexp", sexp);
-			shader.SetUniform1f("u_mshi", mshi);
-			shader.SetUniform1f("u_ks", ks);
-			//shader.SetUniform1i("u_ilum", ilum);
-			shader.SetUniform1i("u_hasBumpTexture", useBumpTexture);
 
-			renderer.Draw(va, ib, shader);
+			for (Model *m : models)
+			{
+				// Draw each model
+				glm::mat4 model;
+				if (m->GetLabel() == "Golfball")
+					model = glm::rotate(m->m_Model, glm::radians(rotation), glm::vec3(1, 0, 0));
+				else
+					model = m->m_Model;
+				glm::mat4 mvp = projection * view * model;
+				glm::mat4 mv = view * model;
+				glm::mat4 invTransMv = glm::inverseTranspose(view * model);
+
+				shader.SetUniformMat4f("u_MVP", mvp);
+				shader.SetUniformMat4f("u_MV", mv);
+				shader.SetUniformMat4f("u_invTransMV", invTransMv);
+
+				m->Bind(shader);
+				shader.SetUniform1i("u_hasBumpTexture", useBumpTexture);
+				m->Draw(shader);
+			}
 
 			rotation += increment;
 
@@ -251,7 +205,7 @@ int main(void)
 					camera.Position = glm::vec3(0);
 				ImGui::End();
 
-				ImGui::Begin("Visuals Properties");
+				ImGui::Begin("Light");
 				ImGui::SliderFloat("Ambient Light Intensity", &globalLightStrength, 0.0f, 1.0f);
 				ImGui::ColorEdit3("Ambient Light Color", glm::value_ptr(globalLightColor)); // Edit 3 floats representing a color
 
@@ -260,26 +214,21 @@ int main(void)
 				ImGui::InputFloat("kl", &kl, 0.05, 0.1);
 				ImGui::InputFloat("kq", &kq, 0.005, 0.01);
 				ImGui::SliderInt("sexp", &sexp, 0, 128);
-
-				ImGui::Text("\nMaterial:");
-				ImGui::InputFloat("mshi", &mshi, 0.5, 10);
-				ImGui::SliderFloat("ks", &ks, 0, 10);
-				ImGui::SliderFloat("kd", &kd, 0, 10);
-				ImGui::SliderFloat("ka", &ka, 0, 10);
-
+				
 				ImGui::Text("\nOther:");
 				ImGui::SliderFloat("rotationSpeed", &increment, 0, 10);
-
-
+				
 				ImGui::End();
-				/*
-								ImGui::Text("mDif");
-								ImGui::SameLine();
-								ImGui::SliderFloat("", &mDif, 0, 1);
-								ImGui::Text("sDif");
-								ImGui::SameLine();
-								ImGui::SliderFloat("", &sDif, 0, 1);
-				  */
+
+				for (Model* m : models)
+				{
+					ImGui::Begin(m->GetLabel().c_str());
+					ImGui::InputFloat("mshi", &(m->m_Mshi), 0.5, 10);
+					ImGui::SliderFloat("ks", &(m->m_Ks), 0, 10);
+					ImGui::SliderFloat("kd", &(m->m_Kd), 0, 10);
+					ImGui::SliderFloat("ka", &(m->m_Ka), 0, 10);
+					ImGui::End();
+				}
 			}
 			ImGui::Render();
 			ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
---
+++ PixelLighting/src/Model.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "VertexArray.h"
+#include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
+#include "Texture.h"
+#include "Renderer.h"
+#include "glm/glm.hpp"
+
+class Model
+{
+private:
+	std::string m_Label;
+	int m_HasAmbientTexture, m_HasDiffuseTexture,
+		m_HasBumpTexture;
+	unsigned int m_AmbientSlot, m_DiffuseSlot, m_BumpSlot;
+	
+	Renderer& m_Renderer;
+	VertexArray *m_Va;
+	VertexBuffer *m_Vb;
+	VertexBufferLayout *m_Layout;
+	IndexBuffer* m_Ib;
+	Texture *m_AmbientTexture, *m_DiffuseTexture, *m_BumpTexture;
+
+public:
+	float m_Mshi, m_Ka, m_Kd, m_Ks;
+	glm::mat4 m_Model;
+
+	Model(const std::string&, const std::string& path, Renderer& renderer);
+	~Model();
+	void Bind(Shader& shader);
+	void Draw(Shader& shader);
+	std::string GetLabel();
+};
